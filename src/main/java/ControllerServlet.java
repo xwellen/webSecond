75cfe320f8f592eafec670de938a7cb317d114da
Sergey Ubozhenko <xwellen@gmail.com>
2021-02-24 21:39:26 +0300

    ControllerServlet logic written

--- src/main/java/ControllerServlet.java
+++ src/main/java/ControllerServlet.java
@@ -1,6 +1,28 @@
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 
 public class ControllerServlet extends HttpServlet {
-
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.setHeader("Access-Control-Allow-Origin", "*");
+        ServletContext servletContext = getServletContext();
+        RequestDispatcher dispatcher;
+        String x = req.getParameter("X");
+        String y = req.getParameter("Y");
+        String r = req.getParameter("R");
+        if (req.getParameterMap().size() == 3 && !x.isEmpty() && !y.isEmpty() && !r.isEmpty()){
+            dispatcher = servletContext.getRequestDispatcher("/area");
+        }
+        else {
+            dispatcher = servletContext.getRequestDispatcher("/index.jsp");
+        }
+        dispatcher.forward(req, resp);
+    }
 }
